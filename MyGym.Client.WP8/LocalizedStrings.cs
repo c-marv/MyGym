68708b400b9cb908652b922423d6415498314563
D'jalmar Gutierrez Titirico <dajaman1@hotmail.com>
2013-11-09 13:08:11 -0400

    Creando Proyectos w8 y wp8 cliente

---
+++ MyGym.Client.WP8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using MyGym.Client.WP8.Resources;
+
+namespace MyGym.Client.WP8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
