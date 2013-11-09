adcac93f6a9095eafafea0939b3e45fa9527de91
D'jalmar Gutierrez Titirico <dajaman1@hotmail.com>
2013-11-09 11:29:29 -0400

    Añadido el controlador para los ejercicios

---
+++ MyGym.Service/Controllers/API/ExerciceController.cs
@@ -0,0 +1,24 @@
+﻿using MyGym.Service.Controllers.API.ErrorHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using MyGym.Service.Models;
+
+namespace MyGym.Service.Controllers.API
+{
+    public class ExcercieController : Controller
+    {
+        [HttpGet]
+        [APIErrorHandler]
+        public JsonResult Get(int userID, bool mode)
+        {
+            var result = new RoutineRepository().GenerateRoutine(userID, mode);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
+
